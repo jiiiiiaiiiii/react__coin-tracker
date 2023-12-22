51198440dfec4c115f3f4b50e821cf7990d42520
aqubi <msp827@naver.com>
2023-12-22 19:40:43 +0900

    Styles
    - Reset CSS
    - createGlobalStyle: 전역스타일 처리

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import styled from 'styled-components';
 
+const Title = styled.h1`
+	color: ${props => props.theme.accentColor};
+`;
 function Coins() {
 	return (
-		<div>Coins</div>
+		<Title>Coin</Title>
 	)
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,9 @@
 import {DefaultTheme} from 'styled-components';
 
 export const theme: DefaultTheme = {
-	bgColor: 'white',
-	textColor: 'black',
-	btnColor: 'tomato',
+	bgColor: '#2f3640',
+	textColor: '#f5f6fa',
+	accentColor: '#4cd137',
 }
 
 
